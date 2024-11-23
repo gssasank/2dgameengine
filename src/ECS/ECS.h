ec08e7ab8187ce1072eb85544f8a6ce002acb70c
Sasank <gssasank@gmail.com>
2024-11-24 02:23:24 +0530

    update: implemented the IPool and Pool classes

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
 
 const unsigned int MAX_COMPONENTS = 32;
 
@@ -43,7 +45,7 @@ class Entity {
         int GetId() const;
 
         bool operator ==(const Entity& otherEntity) const { return id == otherEntity.id; }
-        bool operator !=(const Entity& otherEntity) const { return id =!= otherEntity.id; }
+        bool operator !=(const Entity& otherEntity) const { return id != otherEntity.id; }
 
 }; 
 
@@ -63,15 +65,93 @@ class System {
 
         void AddEntityToSystem(Entity entity);
         void RemoveEntityFromSystem(Entity entity);
-        std::vector<Entity> GetSystemEntities() const; // const added because they only return values and they do not modify anything.
-        const Signature& GetComponentSignature() const; // const added because they only return values and they do not modify anything.
+        std::vector<Entity> GetSystemEntities() const; // const added because they only return values, and they do not modify anything.
+        const Signature& GetComponentSignature() const; // const added because they only return values, and they do not modify anything.
 
         // Defines the component type T that entities must have to be considered by the system.
         template <typename TComponent> void RequireComponent();
 };
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Pool
+////////////////////////////////////////////////////////////////////////////////////////
+// A pool is just a vector of objects of type T (vector is arranged in memory in a contiguous way).
+////////////////////////////////////////////////////////////////////////////////////////
+class IPool {
+    public:
+        virtual ~IPool() {}
+};
+
+template <typename T> // Templates are defined in the header file directly.
+class Pool {
+    private:
+        std::vector<T> data;
+
+    public:
+        Pool(int size = 100) { data.resize(size); }
+
+        virtual ~Pool() = default;
+
+        bool isEmpty() const{
+            return data.empty();
+        }
+
+        int GetSize() const{
+            return data.size();
+        }
+
+        void Resize(int n){
+            data.resize(n);
+        }
+
+        void Clear() {
+            data.clear();
+        }
+
+        void Add(T object){
+            data.push_back(object);
+        }
+
+        void Set(int index, T object){
+            data[index] = object;
+        }
+
+        T& Get(int index){
+            return static_cast<T&>(data[index]);
+        }
+
+        T& operator [](unsigned int index) {
+            return data[index];
+        }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Registry
+////////////////////////////////////////////////////////////////////////////////////////
+// Registry manages the creation and deletion of entities, as well as adding components
+// and systems to entities.
+////////////////////////////////////////////////////////////////////////////////////////
 class Registry {
-     
+    private:
+        // Keep track of how many entities are added to the scene.
+        int numEntities = 0;
+        // A vector of component Pools, each pool contains all the data for a certain component type.
+        // [vector index = ComponentType ID]
+        // [Pool index = Entity ID]
+        std::vector<IPool*> componentPools; // Since Pool class needs a type T, we are going to have a vector of the parent class (interface).
+
+        // Vector of component signatures.
+        // This signature lets us know which components are turned on for an entity.
+        // [vector index = Entity ID]
+        std::vector<Signature> entityComponentSignature;
+
+        // Map of active systems.
+        // [index = SystemType ID]
+        std::unordered_map<std::type_index, System*> systems;
+
+    public:
+        Registry() = default;
+        ~Registry() = default;
 };
 
 template <typename TComponent>
